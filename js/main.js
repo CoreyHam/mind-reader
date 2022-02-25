3490f401038b5b95249c08e2a9cd36a8321da537
Corey <corey.hampton@student.awesomeinc.org>
2022-02-25 02:51:13 +0000

    Update to first working version

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+let answer = "huh";
 let num = 0   
 let currentPage = 0;
 let buttonChecker;
-let array = ["<i class='bi bi-suit-club-fill black'></i>","<i class='bi bi-suit-spade-fill black'></i>","<i class='bi bi-diamond-fill red'></i>","<i class='bi bi-suit-heart-fill red'></i>"]
+let array = ['<i class="bi bi-circle-fill circle"></i>','<i class="bi bi-square-fill square"></i>','<i class="bi bi-triangle-fill triangle"></i>','<i class="bi bi-diamond-fill diamond"></i>','<i class="bi bi-heart-fill heart"></i> ','<i class="bi bi-star-fill star"></i>','<i class="bi bi-cloud-fill cloud"></i>','<i class="bi bi-moon-fill moon"></i>','<i class="bi bi-gear-fill gear"></i>',]
 // IMPORT JSON
 
 
@@ -22,22 +23,25 @@ function updatePages(){
    function doSomething(pages){
       const headerText = document.getElementById("headerText")
       
-      console.log(pages.page[currentPage].nextButton.display)
       document.getElementById("headerText").innerHTML = pages.page[currentPage].headerText
       document.getElementById("nextButton").style.visibility = pages.page[currentPage].nextButton.display
+      document.getElementById("nextButton").innerText = pages.page[currentPage].nextButton.text
       document.getElementById("subText").innerHTML = pages.page[currentPage].subText
       document.getElementById("bottomButton").innerHTML = pages.page[currentPage].bottomButton.text
       buttonChecker = pages.page[currentPage].bottomButton.reset
-      console.log(pages.page[currentPage].symbolHeader)
       if (pages.page[currentPage].symbolHeader != undefined){
          for (let num = 0; num < 100; num++){
             let newDivs = document.createElement("div");
             newDivs.innerHTML = `${num} - ${array[num%array.length]}`; //put in symbol and num
             headerText.appendChild(newDivs);
+            
       }
    }
-}
-   
+      console.log(pages.page[currentPage].answerHeader + "HELLO")
+      if (pages.page[currentPage].answerHeader != undefined){
+            headerText.innerHTML = "Your Symbol " + answer;
+      }
+} 
 // Controller
 
 
@@ -50,17 +54,17 @@ function randomArrayShuffle(array) {
      array[currentIndex] = array[randomIndex];
      array[randomIndex] = temporaryValue;
    }
-   console.log(array);
+   answer = array[0]
  }
 
 function bottomButtonFunc(){
    if(buttonChecker == false){
 
       currentPage++;
-      console.log(currentPage);
       updatePages();
    } else {
       currentPage = 0;
+      randomArrayShuffle(array)
       updatePages();
    }
 }
